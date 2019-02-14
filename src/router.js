db6595e1a4c275c91c151bcd5f3994e634e37dbe
Bok <heybokk@gmail.com>
2019-02-15 08:53:44 +1100

    created two pages and finished router

--- src/router.js
+++ src/router.js
@@ -1,23 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import PageOne from './views/pageOne.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'pageOne',
+      component: PageOne,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/pagetwo',
+      name: 'pageTwo',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/PageTwo.vue'),
     },
   ],
 });
